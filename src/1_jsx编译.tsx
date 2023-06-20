b7ea588e7cd53aeac6a26a16e732b66da8c10a46
liufeiyu <liufeiyu@meituan.com>
2023-06-20 21:03:16 +0800

    fix: 实现第一节demo, 30%

--- src/1_jsx编译.tsx
+++ src/1_jsx编译.tsx
@@ -1,34 +1,51 @@
-import React from "react"
+import React from 'react'
+import { useState } from 'react'
 
-const toLearn = [ 'react' , 'vue' , 'webpack' , 'nodejs'  ]
+const toLearn = ['react', 'vue', 'webpack', 'nodejs']
 
-const TextComponent = ()=> <div> hello , i am function component </div> 
+const TextComponent = () => <div> hello , i am function component </div>
 
-class Index extends React.Component{
-    status = false /* 状态 */
-    renderFoot=()=> <div> i am foot</div>
-    render(){
-        /* 以下都是常用的jsx元素节 */
-        return <div style={{ marginTop:'100px' }}   >
-            { /* element 元素类型 */ }
-            <div>hello,world</div>
-            { /* fragment 类型 */ }
-            <React.Fragment>
-                <div> 👽👽 </div>
-            </React.Fragment>
-            { /* text 文本类型 */ }
-            my name is alien 
-            { /* 数组节点类型 */ }
-            { toLearn.map(item=> <div key={item} >let us learn { item } </div> ) }
-            { /* 组件类型 */ }
-            <TextComponent/>
-            { /* 三元运算 */  }
-            { this.status ? <TextComponent /> : <div>三元运算</div> }
-            { /* 函数执行 */ } 
-            { this.renderFoot() }
-            <button onClick={ ()=> console.log( this.render() ) } >打印render后的内容</button>
-        </div>
-    }
+export default function Index() {
+  const [status, setStatus] = useState(false) /* 状态 */
+  const renderFoot = () => <div> i am foot</div>
+  const testCreate = () => React.createElement('p', null, ' hello,React ')
+  const controlRender = () => {
+    const reactElement = (
+      /* 以下都是常用的jsx元素节 */
+      <div style={{ marginTop: '100px' }}>
+        {/* element 元素类型 */}
+        <div>hello,world</div>
+        {/* fragment 类型 */}
+        <React.Fragment>
+          <div> 👽👽 </div>
+        </React.Fragment>
+        {/* text 文本类型 */}
+        my name is alien
+        {/* 数组节点类型 */}
+        {toLearn.map(item => (
+          <div key={item}>let us learn {item} </div>
+        ))}
+        {/* 组件类型 */}
+        <TextComponent />
+        {/* 三元运算 */}
+        {status ? <TextComponent /> : <div>三元运算</div>}
+        {/* 函数执行 */}
+        {renderFoot()}
+        {testCreate()}
+        <button onClick={() => console.log(controlRender())}>
+          打印render后的内容
+        </button>
+      </div>
+    )
+
+    const { children } = reactElement.props;
+    console.log(children, 'children')
+    
+    const flatChildren = React.Children.toArray(children)
+
+    console.log('flatChildren', flatChildren)
+
+    return reactElement;
+  }
+  return controlRender()
 }
-
-export default Index;
